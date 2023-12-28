4551610db95c990d492dc4a38fcab7ea31e4ef82
Aleksandar Stojanov <me@fnd.works>
2023-12-28 10:05:51 +0100

    requireBucketVersioning
    Signed-off-by: Aleksandar Stojanov <me@fnd.works>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Cloud Storage policies (#9):
   - `disallowPublicBuckets`
   - `requireUniformBucketLevelAccess`
+  - `requireBucketVersioning`
 
 ## [1.2.0] - 2023-12-27
 
--- src/storage/index.ts
+++ src/storage/index.ts
@@ -1,7 +1,9 @@
 import { disallowPublicAccess } from "./disallowPublicAccess";
 import { requireUniformBucketLevelAccess } from "./requireUniformBucketLevelAccess";
+import { requireBucketVersioning } from "./requireBucketVersioning";
 
 export const storagePolicies = [
 	disallowPublicAccess,
+	requireBucketVersioning,
 	requireUniformBucketLevelAccess,
 ];
