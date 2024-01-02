607bb03d0c0f7f8f4261a407512875c307453c40
Aleksandar Stojanov <me@fnd.works>
2024-01-02 18:31:00 +0100

    add PostgreSQL policies
    Signed-off-by: Aleksandar Stojanov <me@fnd.works>

--- src/sql/requireDatabaseFlagsPostgres.ts
+++ src/sql/requireDatabaseFlagsPostgres.ts
@@ -2,46 +2,71 @@ import { ResourceValidationArgs, ReportViolation, EnforcementLevel } from "@pulu
 
 interface ExpectedFlag {
     name: string;
-    value: string;
+    value: string[];
     required: boolean; // 'true' indicates that default value violates the policy
 }
 
-// https://cloud.google.com/sql/docs/mysql/flags
+// https://cloud.google.com/sql/docs/postgres/flags
 const expectedFlags: ExpectedFlag[] = [
     {
-        name: "local_infile",
-        value: "off",
+        name: "log_error_verbosity",
+        value: ["default", "verbose"],
+        required: false,
+    },
+    {
+        name: "log_connections",
+        value: ["on"],
         required: true,
     },
     {
-        name: "skip_show_database",
-        value: "on",
+        name: "log_disconnections",
+        value: ["on"],
+        required: true,
+    },
+    {
+        name: "log_statement",
+        value: ["ddl", "mod", "all"],
+        required: true,
+    },
+    {
+        name: "log_min_error_statement",
+        value: ["debug5", "debug4", "debug3", "debug2", "debug1", "info", "notice", "warning", "error"],
+        required: false,
+    },
+    {
+        name: "log_min_duration_statement",
+        value: ["-1"],
+        required: false,
+    },
+    {
+        name: "cloudsql.enable_pgaudit",
+        value: ["on"],
         required: true,
     },
 ];
 
-export const requireDatabaseFlagsMysql = {
-    name: "sql-require-database-flags-mysql",
-    description: "Check that MySQL Database Instance has database flags set.",
+export const requireDatabaseFlagsPostgres = {
+    name: "sql-require-database-flags-postgres",
+    description: "Check that PostgreSQL Database Instance has database flags set.",
     enforcementLevel: "advisory" as EnforcementLevel,
     validateResource: (args: ResourceValidationArgs, reportViolation: ReportViolation) => {
         if (args.type === "gcp:sql/databaseInstance:DatabaseInstance") {
             const settings = args.props.settings;
             const databaseVersion = args.props.databaseVersion;
-            if (settings && databaseVersion.startsWith("MYSQL")) {
+            if (settings && databaseVersion.startsWith("POSTGRES")) {
                 const flags = settings.databaseFlags;
-                if (!flags) {
-                    expectedFlags.forEach((flag: ExpectedFlag) => {
-                        if (flag.required) {
-                            reportViolation(`Database flag ${flag.name} is missing.`);
-                        }
-                    });
+				if (!flags) {
+					expectedFlags.forEach((flag: ExpectedFlag) => {
+						if (flag.required) {
+							reportViolation(`Database flag ${flag.name} is missing.`);
+						}
+					});
                 } else {
                     flags.forEach((flag: any) => {
                         const expectedFlag = expectedFlags.find((ef) => ef.name === flag.name);
-                        if (expectedFlag && expectedFlag.value !== flag.value) {
+                        if (expectedFlag && !expectedFlag.value.includes(flag.value)) {
 							reportViolation(
-								`Database flag ${flag.name} should be set to '${expectedFlag.value}'.`
+								`Database flag ${flag.name} should be set appropriately.`
 							);
                         }
                     });
