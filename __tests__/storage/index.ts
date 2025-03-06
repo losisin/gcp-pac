c6b831a6a541417e9d34f0a8af8b504e38cd36f8
Aleksandar Stojanov <me@fnd.works>
2025-03-06 08:57:56 +0100

    fix: update storage versioning policy
    Signed-off-by: Aleksandar Stojanov <me@fnd.works>

--- __tests__/storage/index.ts
+++ __tests__/storage/index.ts
@@ -1,6 +1,23 @@
 import * as gcp from '@pulumi/gcp'
 
-export const storageBucket = new gcp.storage.Bucket('fail#1', {
+export const storageBucket1 = new gcp.storage.Bucket('fail#1', {
 	name: 'my-bucket',
-	location: 'europe-west1'
+	location: 'europe-west1',
+	publicAccessPrevention: 'enforced',
+	uniformBucketLevelAccess: true,
+	encryption: {
+		defaultKmsKeyName: 'my-key'
+	},
+	logging: {
+		logBucket: 'my-bucket',
+		logObjectPrefix: 'prefix'
+	}
+})
+
+export const storageBucket2 = new gcp.storage.Bucket('fail#2', {
+	name: 'my-bucket',
+	location: 'europe-west1',
+    hierarchicalNamespace: {
+        enabled: true,
+    },
 })
